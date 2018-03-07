ef36ac9b5a9b8a39f3d38ce0f6b4238218067aed
Kohrak <Gabriel@PPM.local>
2018-03-07 16:15:17 +0000

    add comments to the JS file

--- color.js
+++ color.js
@@ -1,4 +1,5 @@
 function newColors(num){
+  //generates rgb colors and return an array of them
   var colors = [];
   for (var i = 0; i < num; i++){
     var r = Math.floor(Math.random() * 255);
@@ -11,12 +12,14 @@ function newColors(num){
 }
 
 function assignColors(colors, squares){
+  //Changes each html squares background to the colors of the array
   for (var j = 0; j < squares.length; j++){
     squares[j].style.backgroundColor = colors[j];
   }
 }
 
 function win(color){
+  //Make style changes when the user wins
   for (var j = 0; j < squares.length; j++){
     squares[j].style.backgroundColor = color;
   }
@@ -31,16 +34,23 @@ function newGame(){
   } else {
     var num = 3;
   }
+  //generate new colors
   var colors = newColors(num);
+  //assign the colors generated to the html squares
   assignColors(colors, squares);
+  //select a winner color
   winner = colors[Math.floor(Math.random() * num)];
+  //Display the RGB values to guess
   document.querySelector("#colorDisplay").textContent = winner;
+  //Resets the div background color and button message
   panel.style.backgroundColor = "black";
   newGamebutton.textContent = "New Colors";
 }
 
 function logic(){
+  //Checks if the color you selected is the winner color
   if (this.style.backgroundColor == winner){
+    //executes the win dom changes
     win(winner)
   }
   else {
@@ -49,14 +59,25 @@ function logic(){
   }
 }
 
+//declare global variables and element pointers
+
+//variables
 var winner = ""
+var normalMode = true
+
+//pointers
 var squares = document.querySelectorAll(".square");
 var panel = document.querySelector("#gamePanel");
 var newGamebutton = document.querySelector("#newGame")
 var gameMessage = document.querySelector("#gameMessage")
-var normalMode = true
+
+//Adding event listeners
+
 for (var i = 0; i < squares.length; i++){
   squares[i].addEventListener("click", logic);
 }
 newGamebutton.addEventListener("click", newGame)
+
+//initialize
+
 newGame()
